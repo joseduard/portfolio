3dc8abb89bd2c407604c7f4875168d6a2e496920
joseduard <joseduardo.jep@gmail.com>
2022-01-11 13:59:07 +0100

    img to home composant, animations down icon

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./home.scss"
+import React from "react";
+import "./home.scss";
+
+
 
 export default function Home() {
-    return (
-        <div className='home' id='home'>
-            
+  return (
+    <div className="home" id="home">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/assets/photo.png" alt="photo eduardo" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hola et bienvenu, Je suis </h2>
+          <h1>Eduardo Perez </h1>
+          <h3>
+            Développeur web <span>JavaScript</span>
+          </h3>
+        </div>
+        <a href="#realisations">
+            <img src="/assets/down.png" alt="down icon"/>
+      
+
+        </a>
+      </div>
+    </div>
+  );
 }
