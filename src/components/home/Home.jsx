bcbe71b9bd467d1a078e07ee325ab5b6e24aabc0
joseduard <joseduardo.jep@gmail.com>
2022-01-13 18:39:10 +0100

    setting up the ityped extension and using useEffect and useRef to make the home page dynamic

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./home.scss";
-
-
+import { init } from "ityped";
 
 export default function Home() {
+  // Hook useRef fonction ou faite la focntion de un querySelecteur pour choisir un element
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {  //{/*ici on utilise ityped grace au useEffect et useRef, dan ce partie on customise ityped"*/}
+      showCursor: true,
+      backDelay:1500,
+      backSpeed:60,
+      strings: ["JavaScript", "React.js", "Node,js", "Full Stack"],
+    });
+    {
+      /*morceau du code pour utiliser ityped - npm */
+    }
+  }, []);
   return (
     <div className="home" id="home">
       <div className="left">
         <div className="imgContainer">
-          <img src="/assets/photo.png" alt="photo eduardo" />
+          <img src="/assets/Image2.png" alt="photo eduardo" />
         </div>
       </div>
       <div className="right">
@@ -16,13 +29,12 @@ export default function Home() {
           <h2>Hola et bienvenu, Je suis </h2>
           <h1>Eduardo Perez </h1>
           <h3>
-            Développeur web <span>JavaScript</span>
+            Développeur web <span ref={textRef}></span>{" "}
+            {/* ici on utilise  le hook useRef pour faire reference au elenet span*/}
           </h3>
         </div>
         <a href="#realisations">
-            <img src="/assets/down.png" alt="down icon"/>
-      
-
+          <img src="/assets/down.png" alt="down icon" />
         </a>
       </div>
     </div>
