9d92c30233be4acc486c8a074c3a405db9ada0ad
joseduard <joseduardo.jep@gmail.com>
2022-01-17 18:19:45 +0100

    progress in the competences composant

--- src/components/competences/Competences.jsx
+++ src/components/competences/Competences.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import "./competences.scss"
+import React from "react";
+import "./competences.scss";
 
 export default function Competences() {
-    return (
-        <div className='competences' id="competences">
-            
+  return (
+    <div className="competences" id="competences">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/lataupe.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque quaerat pariatur commodi magni ullam quo.
+                </p>
+                <span>no e que poner</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="assets/lataupe.png" alt="" />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt=""/>
+      <img src="assets/arrow.png" className="arrow right" alt=""/>
+    </div>
+  );
 }
